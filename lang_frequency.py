cf297c1bc815900e38b193f046432426cf88bddb
psy4ak <psy4ak@gmail.com>
2016-12-16 21:53:15 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,19 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    text_words = re.findall(r'\w+', open(filepath).read().lower())
+    return text_words
 
 
 def get_most_frequent_words(text):
-    pass
+    top_text_words = Counter(text).most_common(10)
+    return top_text_words
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input('Укажите путь к файлу текста: ')
+    my_text = load_data(file_path)
+    top_words = get_most_frequent_words(my_text)
+    print('Топ 10 слов в тексте и их количество:\n', top_words)
